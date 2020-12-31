8215b8ab731274810d6e26c75eafec60f51b720e
WX-DongXing <dong.xing@outlook.com>
2020-12-31 19:55:28 +0800

    fix: modify vue global props

--- src/index.ts
+++ src/index.ts
@@ -11,28 +11,55 @@ export default {
     app.directive(
       'animation', isVue2 ? AnimationDirectiveV2 : AnimationDirectiveV3,
     );
-    app.prototype.$animateParams = {
-      // timeline max time
-      maxTime: 10000,
-      // store anime instance
-      animates: [],
-      // repeat animate
-      isRepeat: false,
-    };
 
-    app.prototype.$animate = {
-      play: () => {
-        app.prototype.$animateParams.animates.forEach(({ animate }: any) => animate.play());
-      },
-      pause: () => {
-        app.prototype.$animateParams.animates.forEach(({ animate }: any) => animate.pause());
-      },
-      restart: () => {
-        app.prototype.$animateParams.animates.forEach(({ animate }: any) => animate.restart());
-      },
-      seek: (timeStamp: number) => {
-        app.prototype.$animateParams.animates.forEach(({ animate }: any) => animate.seek(timeStamp));
-      },
-    };
+    if (isVue2) {
+      app.prototype.$animateParams = {
+        // timeline max time
+        maxTime: 10000,
+        // store anime instance
+        animates: [],
+        // repeat animate
+        isRepeat: false,
+      };
+
+      app.prototype.$animate = {
+        play: () => {
+          app.prototype.$animateParams.animates.forEach(({ animate }: any) => animate.play());
+        },
+        pause: () => {
+          app.prototype.$animateParams.animates.forEach(({ animate }: any) => animate.pause());
+        },
+        restart: () => {
+          app.prototype.$animateParams.animates.forEach(({ animate }: any) => animate.restart());
+        },
+        seek: (timeStamp: number) => {
+          app.prototype.$animateParams.animates.forEach(({ animate }: any) => animate.seek(timeStamp));
+        },
+      };
+    } else {
+      app.config.globalProperties.$animateParams = {
+        // timeline max time
+        maxTime: 10000,
+        // store anime instance
+        animates: [],
+        // repeat animate
+        isRepeat: false,
+      };
+
+      app.config.globalProperties.$animate = {
+        play: () => {
+          app.config.globalProperties.$animateParams.animates.forEach(({ animate }: any) => animate.play());
+        },
+        pause: () => {
+          app.config.globalProperties.$animateParams.animates.forEach(({ animate }: any) => animate.pause());
+        },
+        restart: () => {
+          app.config.globalProperties.$animateParams.animates.forEach(({ animate }: any) => animate.restart());
+        },
+        seek: (timeStamp: number) => {
+          app.config.globalProperties.$animateParams.animates.forEach(({ animate }: any) => animate.seek(timeStamp));
+        },
+      };
+    }
   },
 };
